d61e794b1906c07da14b53c622960f76d0f8d842
Peter <patamayo@puce.edu.ec>
2024-08-12 23:45:48 -0500

    modificacionvista

--- carroclick/carroclick/settings.py
+++ carroclick/carroclick/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': 'catalogo',
         'HOST': 'localhost',
         'PORT': '5432',
-        'USER': 'cero96',
-        'PASSWORD': '1234'
+        'USER': 'peter1',
+        'PASSWORD': 'pucetec'
     }
 }
 
